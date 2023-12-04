cb5894d6dcf52c26052de759d050f26bf98bf48f
anang shaleh bakti <anang.bakti@gmail.com>
2023-12-04 09:47:31 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+string inputan = "hjjkkloop";
+StringBuilder sb = new StringBuilder();
+string newStr = "";
+string oldStr = "";
+
+
+// loop inputan
+for (int i = 0; i < inputan.Length; i++)
+{
+    newStr = inputan[i].ToString();
+    if (oldStr != newStr) { 
+        sb.Append(newStr);
+    }
+    oldStr = newStr;
+}
+
+
+Console.WriteLine(sb.ToString());
